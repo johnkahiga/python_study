eea14b85ff94907c0d664d4e1a0f1db9cfc9b068
johnkahiga <johnkhaiga27@gmail.com>
2026-04-15 16:43:00 +0300

    second commit

---
+++ extra.py
@@ -0,0 +1,19 @@
+my_name=input('Enter your name: ')
+print(my_name)
+
+num1=input('Enter the first number: ')
+num1=int(num1)
+num2=input('Enter the second number: ')
+num2=int(num2)
+total=num1+num2
+print(total)
+
+number=1000
+print(type(number))
+
+number=str(number)
+print(type(number))
+
+number1=95.23244763
+number2=round(number1,4)
+print(number2)
---
+++ task2.py
@@ -0,0 +1,17 @@
+temp=56.8926
+temp=round(temp)
+print(temp)
+
+temp=56.8926
+temp=round(temp,2)
+print(temp)
+
+temp=56.8926
+temp=round(temp,3)
+print(temp)
+
+temp=56.8926
+temp=str(temp)
+temp=temp[3]+temp[2]+temp[4:]
+temp=float(temp)
+print(temp)
