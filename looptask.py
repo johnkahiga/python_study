4f681dc53d238c827ed122202f53b4639599fb5c
johnkahiga <johnkhaiga27@gmail.com>
2026-04-30 18:14:30 +0300

    looptask

---
+++ looptask.py
@@ -0,0 +1,61 @@
+# Write a program that displays a numbers 1 to 50 inside a list.
+# lst=list(range(1,51))
+# print(list)
+# # From 1 above display the ones divisible by 7 or 5 inside a list.
+# num=[]
+# for i in lst:
+#     if i%7==0 or i%5==0:
+#         num.append(i)
+
+# print(num)
+
+# # Find sum and average of values in the range between 10 to 40.
+# val=list(range(10,41))
+# total=0
+# count=0
+# for i in val:
+#     total+=i
+#     count+=1
+# print(total)
+# print(count)
+# average=total/count
+# print(average)   
+
+    
+# # Put in a list the first 10 odd numbers between 10 to 50.
+# odd=list(range(10,51))
+# odds=[]
+# for i in odd:
+#     if i%2 !=0:
+#         odds.append(i)
+# print(odds)
+
+# # write a program that takes a number as input and prints its multiplication table up to 10 using a for loop.
+# num=int(input('Enter a number:'))
+# lst=list(range(1,11))
+# for i in range(1,11):
+#      result=num*i
+# print(num,'X',i,'=',result) 
+
+# write a program that counts and prints the number of even numbers between 1 and 50 using a for loop
+val=[]
+lst=list(range(1,51))
+count=0
+for i in lst:
+    if i%2==0:
+        count+=1
+        val.append(i)
+print(count)
+print(val)
+
+
+
+# ls1 = [ (“Jay”, ‘20’), (“Mo”, ‘30’), (“Mya”, ‘32’) ]
+# Display the total quantity of the 3 above.
+ls1 =[('Jay','20'),('Mo','30'),('Mya', '32')]
+total=0
+for i, y in ls1:
+    total=total + int(y)
+print(total)
+
+
